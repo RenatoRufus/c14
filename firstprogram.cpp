925b15a6ece433e4fd22ba17e3ae3e208384d5d7
RenatoRufus <renatooffice1@gmail.com>
2023-12-27 21:37:17 -0300

    .

---
+++ firstprogram.cpp
@@ -0,0 +1,13 @@
+#include <iostream> //Diretivas pré-processador
+
+int main(){
+ 
+    int favorite_number;
+
+    std::cout<<"Enter you favorite number between 1 and 100\n";
+    std::cin>> favorite_number;
+    std::cout<<"Amazing!! That's my favorite number too!"<<std::endl;
+    
+
+    return 0;
+}
