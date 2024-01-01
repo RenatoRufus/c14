4d6e0e5345a5f8751bc42607b6c591c0e8452fbd
RenatoRufus <renatooffice1@gmail.com>
2024-01-01 19:35:09 -0300

    .

---
+++ arquivos/variinit2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+extern int x;
+
+int main(){
+
+    std::cout<<"Hello \n"<<std::endl;
+    std::cout<< x << "\n";
+
+
+    return 0;
+}
---
+++ arquivos/varinit.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main(){
+
+    int miles_driven;
+
+    std::cout<< miles_driven;  //variável não inicializada 
+                               //retorna lixo
+
+
+    int number;
+    number = 100;                            
+
+
+
+    return 0;
+}
