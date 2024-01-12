8ec11a3745ca58a72daaec395cc0a518ff97b8ce
RenatoRufus <renatooffice1@gmail.com>
2024-01-12 20:51:34 -0300

    .

---
+++ namespace.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+//using namespace std;  //use the entire std namespace
+
+using std::cout;
+using std::cin;
+
+
+
+int main(){
+
+    int favorite = 10;
+
+    cout<< favorite <<std::endl; // não utilizando namespace em endl
+    
+    
+
+
+    return 0;
+}
+
