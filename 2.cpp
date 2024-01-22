f3a5b972ef7b8866952b3d3c172dd50c84715ad7
RenatoRufus <renatooffice1@gmail.com>
2024-01-22 20:23:17 -0300

    .

---
+++ 2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+ using namespace std;
+
+int main(){
+
+   int room_width {0}; 
+   int room_length {0};
+
+   cout<<"Enter the width of the room: "<<endl;
+   cin>>room_width;
+
+   cout<<"Enter the lenght of the room: "<<endl;
+   cin>>room_length; 
+
+   cout<<"The area of the room is: "<< room_width * room_length <<"square feet"<< endl;
+
+
+
+
+   return 0;
+}
