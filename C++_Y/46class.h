dfa5f31d1e75a7c8d6c4eaf133536831ff702dc9
RenatoRufus <renatooffice1@gmail.com>
2024-07-31 20:41:50 -0300

    .

---
+++ C++_Y/46class.h
@@ -0,0 +1,20 @@
+#ifndef CLASSES_H_INCLUDED
+#define CLASSES_H_INCLUDED
+
+
+
+class Veiculo{
+public:
+    int velMax;
+    int tipo;
+    
+private:
+    string nome;
+    int vel;
+    bool ligado;
+
+};
+
+
+
+#endif  //CLASSES_H_INCLUDED
