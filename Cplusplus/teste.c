c0f36f82308743580627b5a5dbb2fe48139b743a
RenatoRufus <renatooffice1@gmail.com>
2024-09-08 18:55:49 -0300

    .

---
+++ Cplusplus/teste.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(int charc, char *charv[])
+{
+    printf("Content-type: text/plain\n\n");
+    printf("Olá mundo!");
+
+
+
+    return 0;
+}
+
+/*   gcc -o hello hello.c  */
