26caf8e82507a19714507aaedbf9451d8ec374da
RenatoRufus <renatooffice1@gmail.com>
2024-10-10 18:06:39 -0300

    .

--- C++_Y/1.cpp
+++ C++_Y/1.cpp
@@ -1,18 +1,10 @@
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 
 int main(){
 
-   
-   n=10
-   if (n < 3)
-        n=n-1
-   else:
-    if n+10 > 20:
-    n=n**2-1
-   
-print(n)
+ cout<<"Hello World!";
 
     return 0;
 }
