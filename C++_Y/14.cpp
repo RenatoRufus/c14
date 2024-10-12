600d522ce09e145c8cd58d9549360a0a0ecbf90e
RenatoRufus <renatooffice1@gmail.com>
2024-10-12 13:38:02 -0300

    .

---
+++ C++_Y/14.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    //Laço while
+
+    int n=0;
+
+    while(n<10){         //enquanto n for menor que 10 execute
+        cout<< n <<"\n";
+        //n++
+        cin>>n;
+    }
+
+    cout<<"Rotina finalizada\n";
+
+    int cont = 0;
+    while (cont < 20)
+    {
+        cout<<"escrita\n";
+        cont++;
+    }
+
+    cout<<"Rotina finalizada\n";
+
+    
+
+
+
+
+
+    return 0;
+}
