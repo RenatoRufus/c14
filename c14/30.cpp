1c9116cb498fa66e549ae1eefce441ef50f569bc
RenatoRufus <renatooffice1@gmail.com>
2024-10-15 20:18:39 -0300

    .

---
+++ c14/30.cpp
@@ -0,0 +1,13 @@
+//Warning compiler
+
+#include <iostream>
+
+int main(){
+
+    int favorite_number; //variável não inicializada
+
+    std::cout<< favorite_number <<std::endl;
+
+
+    return 0;
+}
---
+++ testes/2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int a = 4;
+    int b = 6;
+    int soma = a + b;
+    cout<<"soma\n";
+    cout<< soma <<"\n"<<endl;
+    cout<< a + b <<"\n";
+    cout<<"a + b";
+
+
+    return 0;
+}
