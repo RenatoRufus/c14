66915167c0f6ebee2c85759d0a053dafb30e36dc
RenatoRufus <renatooffice1@gmail.com>
2024-10-19 18:09:51 -0300

    .

---
+++ C++_Y/18c.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int tam2 = 6;
+    int vetor[tam2] = {1222,21,31,41,51,2};
+    int d;
+
+    for(d = 0; d < tam2; d++){
+        cout<< vetor[d] <<"\n";
+    }
+
+
+    return 0;
+}
