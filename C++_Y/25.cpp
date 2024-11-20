7a0176022c96a9fae6f1d050d9df28d40117572d
RenatoRufus <renatooffice1@gmail.com>
2024-11-20 20:07:09 -0300

    .

---
+++ C++_Y/25.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+void contador(int num, int cont=0);
+
+int main(){
+    //funções recursivas
+    contador(20);
+
+    return 0;
+}
+
+void contador(int num, int cont){
+    cout<< cont << "\n";
+    if(num > cont){
+        contador(num, ++cont);
+    }
+
+}
+
+/*
+void contador(int num){
+    for(int i = 0; i <= num; i++ ){
+    cout<< i << "\n";
+    }
+}
+
+
+
+*/
