61a6803711acd2737be585679692be602bc324e7
RenatoRufus <renatooffice1@gmail.com>
2024-11-25 22:10:05 -0300

    .

---
+++ c14/37.cpp
@@ -0,0 +1,18 @@
+//namespaces 
+#include <iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main(){
+    int favorit_number;
+
+    cout<<"Digite seu número favorito\n";
+    cin>>favorit_number;
+
+    cout<< favorit_number <<"\n";
+
+
+    return 0;
+}
