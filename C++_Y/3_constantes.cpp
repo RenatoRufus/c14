114dfb8b98b5e1c9c7a4571de70b96292319b774
RenatoRufus <renatooffice1@gmail.com>
2025-04-28 21:09:32 -0300

    .

---
+++ C++_Y/3_constantes.cpp
@@ -0,0 +1,21 @@
+/*
+Declarações múltiplas de variáveis, Constantes #Define
+*/
+#include <iostream>
+
+using namespace std;
+
+#define pi 3.1415
+#define canal cout<<"Canal fessor bruno\n\n";
+
+int main(){
+
+    int vidas=3, tiros=500, life=100;
+
+    cout<< pi <<"\n\n";   
+    canal 
+
+
+
+    return 0;
+}
