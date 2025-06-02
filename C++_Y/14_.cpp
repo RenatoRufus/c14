fc8661555a4281f22774bffafad5121a181addbd
RenatoRufus <renatooffice1@gmail.com>
2025-06-02 20:27:21 -0300

    .

--- C++_Y/14_.cpp
+++ C++_Y/14_.cpp
@@ -5,11 +5,19 @@ using namespace std;
 int main(){
 
     int n=0;
+    int cont=0;
 
-    while( n<10){
-        cout<< n <<"\n";
+    while( cont++<20){
+        cout<< "Canal " << cont <<"\n";
+        //n++;
+        //cin>>n;
+        //cont++;
+        //cont+=2;
+        if(cont == 10){
+            break;
+        } 
     }
-
+    cout<<"\nRotina finalizada\n";
 
     return 0;
 }
