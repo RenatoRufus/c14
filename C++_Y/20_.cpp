d82d826d721bbbca8f30f0c93143cb8b8b163040
RenatoRufus <renatooffice1@gmail.com>
2025-06-10 19:28:33 -0300

    .

--- C++_Y/20_.cpp
+++ C++_Y/20_.cpp
@@ -1,11 +1,26 @@
 #include <iostream>
+#include <stdlib.h>
+#include <cstdlib>
+
 
 using namespace std;
 
 int main(){
 
     char palavra[30], letra[1], secreta[30];
-    int tam, i, chances, acertos; 
+    int tam, i, chances, acertos;
+    bool acerto = false;
+    
+    chances=6;
+    tam=0;
+    i=0;
+    acerto=false;
+    acertos=0;
+
+    cout<<"Fale para seu amigo tampar os olhos\n";
+    cin>>palavra;
+
+    system("Clear");
 
 
 
