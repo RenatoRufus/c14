3e6b7c8c4f5c8155cf4c7bdcdff969ac747130f1
RenatoRufus <renatooffice1@gmail.com>
2025-06-16 21:03:05 -0300

    .

---
+++ C++_Y/23B_.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+void soma();
+
+int main(){
+
+    soma();
+
+    return 0;
+}
+
+
+void soma(){
+
+    int n1,n2,re;
+    n1=10;
+    n2=20;
+    re=n1+n2;
+    cout<<"Soma de "<< n1 <<" com " << n2 << " = " << re;
+}
