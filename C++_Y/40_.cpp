f3d6bbda2cc65f1060c08c08848025bf50dfc260
RenatoRufus <renatooffice1@gmail.com>
2025-07-22 20:40:52 -0300

    .

---
+++ C++_Y/40_.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main(){
+
+    int num1=123;
+
+    printf("Hello printf\nCurso c++");
+    //printf("\0"); \0não funcionou
+
+    printf("Valor da variavel num:1 %d", num1);
+
+
+    return 0;
+}
