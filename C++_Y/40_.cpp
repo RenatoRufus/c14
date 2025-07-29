5da67cb35e74e5e2c9d5202ce928d4ed277e8cc1
RenatoRufus <renatooffice1@gmail.com>
2025-07-29 20:54:52 -0300

    .

--- C++_Y/40_.cpp
+++ C++_Y/40_.cpp
@@ -5,13 +5,36 @@ using namespace std;
 
 int main(){
 
-    int num1=123;
+    int num1=1, num2=2, num3=3;
+    char nome[10]="bruno";
+    /*
+    nome[0] ='B';
+    nome[0] ='r';
+    nome[0] ='u';
+    nome[0] ='\0';
+    */
+
+    scanf("%d,&num1");
+
 
     printf("Hello printf\nCurso c++");
     //printf("\0"); \0não funcionou
 
-    printf("Valor da variavel num:1 %d", num1);
+    printf("Valor da variavel num:1 %d %d %d", num1,num2,num3);
+    printf("Valor da variavel:  %d \n Nome: %s\n", num1, nome);
+
+
 
 
     return 0;
 }
+
+/*
+d,i => int
+x,X => Hexadecimal
+u => int sem sinal
+s => string, char
+f => double
+p => ponteiros
+
+*/
