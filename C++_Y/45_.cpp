e13dee0a85861ff7fee23bd51574bd6acfb31171
RenatoRufus <renatooffice1@gmail.com>
2025-09-29 20:02:20 -0300

    .

--- C++_Y/45_.cpp
+++ C++_Y/45_.cpp
@@ -1,11 +1,16 @@
 //POO, Classe em arquivo externo, Construtor 
 #include <iostream>
+#include "aviao.h"
 
 using namespace std;
 
 int main(){
 
+    Aviao *av1=new Aviao(3);
+    Aviao *av2=new Aviao(1);
+    Aviao *av3=new Aviao(2);
 
+    av1->imprimir();
 
 
 
--- C++_Y/aviao.h
+++ C++_Y/aviao.h
@@ -9,6 +9,7 @@ public:
     int velMax;
     std::string tipo;
     Aviao(int tp);
+    void imprimir();
     
     private:
 
@@ -16,10 +17,28 @@ public:
 
 Aviao::Aviao(int tp){
     if(tp==1){
-        
+       tipo="Jato"; 
+       velMax=800;
+    }else if(tp==2){
+        tipo="Monomotor";
+        velMax=350;
+    }else if (tp==3){
+        tipo="Planador";
+        velMax=180;        
     }
 
 }
+
+void Aviao imprimir(){
+
+    std::cout<<"Tipo.............: "<< tipo << endl;
+    std::cout<<"Velocidade máxima: "<< velMax << endl;
+    std::cout<<"Velocidade atual.: "<< vel << endl;
+
+}
+
+
+
 
 #endif
 
