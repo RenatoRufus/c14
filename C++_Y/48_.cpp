8461b77d4c6a77038c467f9fd05a4555ac15e4bd
RenatoRufus <renatooffice1@gmail.com>
2025-10-14 21:01:31 -0300

    .

---
+++ C++_Y/48_.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "classes.h"
+
+//POO, Classes, Herança Múltipla 
+
+using namespace std;
+
+int main(){
+
+    Base1 *obj1 = new Base1();
+
+
+    obj1->impBase1();
+
+    return 0;
+
+
+}
