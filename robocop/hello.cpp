db26843b1633a066d9923686266f1799eb968534
RenatoRufus <renatooffice1@gmail.com>
2025-10-15 22:09:12 -0300

    .

--- robocop/hello.cpp
+++ robocop/hello.cpp
@@ -1,10 +1,19 @@
 #include <iostream>
+/*
+A diretiva de pré-processador #include
+indica para incluir o arquivo de cabeçalho
+iostream
 
-int main(){
+*/
+int main(){ //função principal main
+        system("color 5f");
+
     std::cout<<"Hello world!!!, Enter para continuar...";
-    std::cin.get();
     
-
+    std::cin.get();
+    //cout - comando de saída de dados
+    //endl - insere caractere quebra de linha
+    //ide - ambiente de desenvolvimento integrado
 
     return 0;
-}
+}//chaves inicio e fim de um bloco
