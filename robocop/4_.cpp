f15b16cca6c8f6a8c71ffc6bd64207ab958c9da0
RenatoRufus <renatooffice1@gmail.com>
2025-10-28 19:51:00 -0300

    ,

---
+++ robocop/4_.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#define pulelinha "\n" //#define o que será substituido
+
+using namespace std;
+
+int main(){
+
+    typedef char caractere;    //char == caractere renomeia ( apelido )
+    caractere Letra = 'C';
+
+    cout<<"Hello world, c++"<< pulelinha;
+
+
+
+
+    return 0;
+}
