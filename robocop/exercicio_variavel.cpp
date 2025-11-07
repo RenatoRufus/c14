3f226b4c3985a907d65d944fc1aa895746269617
RenatoRufus <renatooffice1@gmail.com>
2025-11-07 20:57:08 -0300

    .

---
+++ robocop/exercicio_variavel.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int numcomputadores = 100;
+    int serialcomputadores =111111;
+
+    cout<<"$$$$$$$$$$$$$$$$$$$$$$$$$$$"<<endl;
+    cout<<"\tValor numcomputadoes" << numcomputadores <<"\n";
+    cout<<"\tValor serialcomputadores" << serialcomputadores <<"\n";
+    cout<<"$$$$$$$$$$$$$$$$$$$$$$$$$$$"<<endl;
+
+
+    return 0;
+}
---
+++ robocop/tipos_variaveis.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int numero;
+    numero = 45;
+
+
+    cout<<"Numero: "<< numero <<endl;
+    cout<<"Tamanho variavel numero: "<< sizeof(numero)<<"\n";
+    cout<<""
+
+
+    return 0;
+}
