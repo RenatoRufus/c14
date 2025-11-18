9f2dad3920e1ebc03bb501b51539bd5743e8d126
RenatoRufus <renatooffice1@gmail.com>
2025-11-18 20:36:04 -0300

    .

---
+++ robocop/recebendo_dados.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int numero, numero2;
+    cout<<"Digite o primeiro numero";
+    cin>>numero;
+    cout<<"Digite o segundo numero";
+    cin>>numero2;
+
+
+
+    return 0;
+}
