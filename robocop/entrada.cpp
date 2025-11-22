cee528e43a66a57863935c159fecf50cdba234e2
RenatoRufus <renatooffice1@gmail.com>
2025-11-22 16:27:38 -0300

    .

---
+++ robocop/entrada.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int num1, num2, num3, num4;
+
+    cout<<"Digite Quatro numeros sepados por espaco e pressione ENTER: ";
+
+    cin>>num1 >> num2 >> num3 >> num4;
+    cout<<"\nSoma = " << (num1 + num2 + num3 + num4)<<endl;
+    
+    
+    return 0;
+}
