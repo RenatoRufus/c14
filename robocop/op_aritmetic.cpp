f94fa8de934b925785e8f396746902df59046256
RenatoRufus <renatooffice1@gmail.com>
2025-11-25 21:12:01 -0300

    .

---
+++ robocop/op_aritmetic.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int num = 3, num2 = 2, num3, num4;
+    int soma, subtração;
+
+    soma = num + num2;
+    cout<<"Soma "<< soma <<endl;
+
+    cout<<"Insira o numero 3 \n";
+    cin>>num3;
+
+    cout<<"Insira o numero 4 \n";
+    cin>>num4;
+
+    subtração = num3 - num4;
+
+    cout<<"A subtração dos números "<< subtração <<"\n";
+
+
+
+    return 0;
+}
