89fb38c6ef8dd046dc2b961376eda14d5c71822e
RenatoRufus <renatooffice1@gmail.com>
2025-12-08 20:16:11 -0300

    .

---
+++ robocop/operadoreslogico.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main(){
+
+    double Numero;
+    bool maiorque100;
+
+    std::cout<<"Digite um número: \n";
+    std::cin>>Numero;
+    std::cout<<"True = 1 e False = 0"<<"\n";
+
+    maiorque100 = (Numero > 100);
+    std::cout<<"O resultado é maior que 100 :"<<maiorque100 <<"\n";
+
+
+
+
+
+
+}
